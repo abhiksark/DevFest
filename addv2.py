be56640e1d844bcd288b2d9cc92806e7af404e59
Abhik <abhiksark@gmail.com>
2022-12-12 22:38:09 +0530

    first commit

---
+++ addv2.py
@@ -0,0 +1,5 @@
+def test(x):
+    cdef int y = 0
+    for i in range(x):
+        cdef int y += cdef int i
+    return y
---
+++ addv4.pyx
@@ -0,0 +1,6 @@
+cdef int test(int x):
+    cdef int y = 0
+    cdef int i
+    for i in range(x):
+        y +=i
+    return y
